f87673976ece49b41dbbd75c58f6db458ce87bf1
Amit Chaudhary <chaudharyamitiit2007@gmail.com>
2015-04-25 17:27:15 +0530

    initialize the bookmark project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bookmarks::Application.config.secret_token = '5a1a430224d2f9d9eb4d33efe9ffa6f85a857b041753009d84615549d479e95ea0ea2a9e95ee53ad076898c40573dd2085989c4e3e85d68a12128189e7da690c'
